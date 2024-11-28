e01951d0735867f18f4b59be1384aa8ab549e2c3
mrterminal-git <121609885+mrterminal-git@users.noreply.github.com>
2024-11-28 11:03:03 +1000

    Build: initial commit
    Project for parsing the epitrend data

---
+++ include/Common.hpp
@@ -0,0 +1,22 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <unordered_map>
+#include <cmath>
+#include <numeric>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib> 
+#include <functional>
+#include <filesystem>
+#include <tuple>
+#include <exception>
+
+#include "URT.hpp"
+
+#endif // COMMON_HPP
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "Common.hpp"
+#include "FileReader.hpp"
+#include "Config.hpp"
+
+int main() {
+
+return 0;
+}
