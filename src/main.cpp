006923c0cd669c873ce2eba135d0067f2efa3c32
mrterminal-git <121609885+mrterminal-git@users.noreply.github.com>
2024-12-02 10:08:56 +1000

    refactor: adjust main for new DB and config for work point toward data folder

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,27 @@ int main() {
 
 std::string connectionString = 
     "Driver=ODBC Driver 18 for SQL Server;"
-    "Server=tcp:epitrenddbts.database.windows.net,1433;"
-    "Database=EpitrendDBTS;"
-    "Uid=mrterminal;"
-    "Pwd=$Newmonogatar1;"
+    "Server=tcp:au-eng-mbe.database.windows.net,1433;"
+    "Database=TimeSeries_UVData;"
+    "Uid=AU-Engineer-Admin;"
+    "Pwd=f7rid4bTBd2JmeDWeVdH;"
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
     "Connection Timeout=30;";
 
 std::string query_table_name = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE';";
 std::string query_create_table = 
-    "CREATE TABLE epitrend_data_3 ( "
+    "CREATE TABLE epitrend_data_time_series ( "
     "id BIGINT NOT NULL IDENTITY(1,1), "
     "name VARCHAR(255), "
     "date_time DATETIME2, "
-    "value FLOAT, "
+    "value DECIMAL, "
     "PRIMARY KEY (id) );";
 std::string query_table_contents = "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH "
                                    "FROM INFORMATION_SCHEMA.COLUMNS "
-                                   "WHERE TABLE_NAME = 'epitrend_data_3';";
-std::string query_table_all = "SELECT * FROM epitrend_data_3";
-std::string query_table_delete_all = "DELETE FROM epitrend_data_3";
+                                   "WHERE TABLE_NAME = 'epitrend_data_time_series';";
+std::string query_table_all = "SELECT * FROM epitrend_data_time_series";
+std::string query_table_delete_all = "DELETE FROM epitrend_data_time_series";
 
 AzureDatabase db;
 
@@ -41,22 +41,22 @@ if (!db.connect(connectionString)) {
     std::cout << "Connection to database established!\n";
 }
 
-// // Execute a query
-// if (db.queryExecute(query_table_all)) {
-//     std::cout << "Query executed successfully!" << std::endl;
-// } else {
-//     std::cerr << "Query execution failed." << std::endl;
-// }
-
-EpitrendBinaryData binary_data;
-for (int i = 0; i < 4; i++) {
-    FileReader::parseEpitrendBinaryDataFile(binary_data,2024,11,1,i,false);
-    std::cout << "Current size of EpitrendBinaryData object: " << binary_data.getByteSize() << "\n";
+// Execute a query
+if (db.queryExecute(query_table_contents)) {
+    std::cout << "Query executed successfully!" << std::endl;
+} else {
+    std::cerr << "Query execution failed." << std::endl;
 }
-binary_data.printFileAllTimeSeriesData("temp.txt");
 
-// copy all data into SQL table
-db.copyToSQL("epitrend_data_3", binary_data);
+// EpitrendBinaryData binary_data;
+// for (int i = 0; i < 4; i++) {
+//     FileReader::parseEpitrendBinaryDataFile(binary_data,2024,11,1,i,false);
+//     std::cout << "Current size of EpitrendBinaryData object: " << binary_data.getByteSize() << "\n";
+// }
+// binary_data.printFileAllTimeSeriesData("temp.txt");
+
+// // copy all data into SQL table
+// db.copyToSQL("epitrend_data_time_series", binary_data);
 
 return 0;
 }
